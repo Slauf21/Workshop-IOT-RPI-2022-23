29cbce1a29b50267ed9ffc7a48e7ef86afe8dd89
Ferdi Ileri <12001728@student.pxl.Be>
2022-11-07 12:08:16 +0100

    commentaar toevoegen

--- Python_bot/bot.py
+++ Python_bot/bot.py
@@ -1,3 +1,4 @@
+# Libs
 import telebot
 import os
 from picamera import PiCamera, Color
@@ -14,24 +15,32 @@ bot = telebot.TeleBot(API_KEY)
 camera = PiCamera()
 camera.resolution = (640,480)
 
-#DHT declareren
+# DHT declareren
 dht_sensor = Adafruit_DHT.DHT11
-gpio = 18 #GPIO voor dht sensor
+# GPIO voor dht sensor
+gpio = 18
 
+# Commondo voor simpele reply
 @bot.message_handler(commands=['hallo'])
 def hallo(message):
     bot.reply_to(message, "Hi from TELEBOT!!!!!!")
 
+# Commondo voor foto
 @bot.message_handler(commands=['foto'])
 def hallo(message):
     bot.reply_to(message, "Foto is onderweg...")
+    # Capture
     camera.capture('/home/pem/Desktop/pem_python/image.jpg')
+    # Foto sturen via telegram
     bot.send_photo(chat_id, photo=open('/home/pem/Desktop/pem_python/image.jpg', 'rb'))
     
+# Commondo voor DHT
 @bot.message_handler(commands=['temperatuur'])
 def hallo(message):
     bot.reply_to(message, "Temperatuur wordt gemeten...")
-    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio)    
+    # Meting
+    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, gpio)
+    # Meting sturen via telegram
     bot.reply_to(message, 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 
 bot.polling()
