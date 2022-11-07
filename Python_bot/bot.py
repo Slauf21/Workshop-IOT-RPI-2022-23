3c9411d8b7921fb39220e350a0983c3b91be1d5d
Ferdi Ileri <12001728@student.pxl.Be>
2022-11-07 11:04:15 +0100

    kan foto maken en bewaren onder bestanden

--- Python_bot/bot.py
+++ Python_bot/bot.py
@@ -1,17 +1,25 @@
-import requests
 import telebot
 import os
+from picamera import PiCamera, Color
+
 TOKEN = "5574596927:AAE4WXBSZbe0N6eIdlxHIfKe32hQfjJEThE"
 API_KEY = '5574596927:AAE4WXBSZbe0N6eIdlxHIfKe32hQfjJEThE'
 chat_id = "5452220589"
-#message = "slauf"
-#url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-#print(requests.get(url).json()) # this sends the message
+
+# Resolutie camera instellen
+camera = PiCamera()
+camera.resolution = (640,480)
 
 bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['hallo'])
 def hallo(message):
     bot.reply_to(message, "Hi from TELEBOT!!!!!!")
+
+@bot.message_handler(commands=['foto'])
+def hallo(message):
+    bot.reply_to(message, "Foto is onderweg...")
+    camera.capture('/home/pem/Desktop/pem_python/image.jpg')
+    
 
 bot.polling()
